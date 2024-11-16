84d7c0bb2fd99de256b077dc473072822b786196
krishna behera <krishnakantbehera28@gmail.com>
2024-11-16 21:15:39 +0530

    Sign-Up button change

--- src/Components/Ordersuccess.jsx
+++ src/Components/Ordersuccess.jsx
@@ -3,7 +3,7 @@ function Ordersuccess() {
   return (
     <div className="Ordersuccess">
       <img
-        src="https://cdn.iconscout.com/icon/premium/png-512-thumb/chauffeur-3-774557.png?f=webp&w=512"
+        src="https://cdn.iconscout.com/icon/premium/png-512-thumb/loacation-6838745-5599676.png?f=webp&w=512"
         alt="order"
       />
       <h4>Order Successfull</h4>
